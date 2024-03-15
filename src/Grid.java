7a048a160af13720746087f7883346a8269d00ab
BT_2N2_28 <sample@bths.edu>
2024-03-15 13:02:40 -0400

    commit 1:02

--- src/Grid.java
+++ src/Grid.java
@@ -7,6 +7,7 @@ public class Grid {
     private Pellets pellet;
     private Ghost ghost;
     private Block block;
+    private Space[][] grid;
 
     public Grid() {
         pellet = new Pellets(5);
@@ -14,15 +15,54 @@ public class Grid {
         playerDown = new Player("^");
         playerLeft = new Player(">");
         playerRight = new Player("<");
-        space = new Space(" ");
+        space = new Space("_");
     }
 
     public void gridSetup() {
-        Space[][] grid = new Space[10][10];
+        grid = new Space[10][10];
         for (int i = 0; i < grid.length; i++) {
             for (int k = 0; k < grid[i].length; k++) {
-                grid[i][k] = pellet;
+                grid[i][k] = space;
             }
+        }
+        for (int col = 0; col < 6; col++) {
+            grid[0][col] = block;
+        }
+        for (int row = 0; row < 5; row++) {
+            grid[row][5] = block;
+        }
+        for (int row = 4; row < 7; row++) {
+            for (int col = 1; col < 5; col++) {
+                grid[row][col] = block;
+            }
+        }
+        for (int col = 1; col < 9; col++) {
+            grid[8][col] = block;
+        }
+        for (int row = 4; row < 7; row++) {
+            grid[row][8] = block;
+        }
+        grid[4][7] = block;
+        grid[6][9] = block;
+        grid[9][5] = playerRight;
+        grid[6][6] = ghost;
+        for (int i = 0; i < 20; i++) {
+            int row = (int) (Math.random() * 10);
+            int col = (int) (Math.random() * 10);
+            if (grid[row][col] == space) {
+                grid[row][col] = pellet;
+            } else {
+                i--;
+            }
+        }
+    }
+
+    public void printGrid() {
+        for (int col = 0 ; col < grid.length; col++) {
+            for (int row = 0; row < grid[0].length; row++) {
+                System.out.print(grid[row][col] + " ");
+            }
+            System.out.println();
         }
     }
 }
@@ -33,10 +73,8 @@ O O O O O █ O O O O
 _ _ _ _ _ █ _ _ _ _
 _ █ █ █ █ █ _ █ █ _
 _ █ █ █ █ _ O _ █ _
-_ █ █ █ █ _ G _ █ _
+_ █ █ █ █ _ G _ █ █
 _ _ _ _ _ _ O _ _ _
 _ █ █ █ █ █ █ █ █ _
 _ _ _ _ > _ _ _ _ _
-
-
  */
